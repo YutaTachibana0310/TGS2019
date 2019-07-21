d3ed808a845d0fffb86a2bb753bf25d5241926df
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-07-21 18:49:22 +0900

    プロジェクト ファイルを追加します。

---
+++ GameScene.h
@@ -0,0 +1,29 @@
+//=====================================
+//
+//ゲームシーンヘッダ[GameScene.h]
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#ifndef _GAMESCENE_H_
+#define _GAMESCENE_H_
+
+#include "main.h"
+#include "Framework\IStateScene.h"
+
+/**************************************
+マクロ定義
+***************************************/
+
+/**************************************
+クラス定義
+***************************************/
+class GameScene : public IStateScene
+{
+public:
+	void Init() override;
+	void Uninit() override;
+	void Update(HWND hWnd) override;
+	void Draw() override;
+};
+
+#endif
