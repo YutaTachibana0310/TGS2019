acb9cca7b46d07429b2772d8f86e210a1a37e337
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-07-23 10:59:39 +0900

    ResourceManagerにテクスチャ管理機能を追加

--- Framework/ResourceManager.cpp
+++ Framework/ResourceManager.cpp
@@ -62,6 +62,54 @@ bool ResourceManager::GetMesh(const char* tag, MeshContainer** pOut)
 }
 
 /**************************************
+テクスチャ読み込み処理
+***************************************/
+void ResourceManager::LoadTexture(const char *tag, const char* path)
+{
+	string tagStr = string(tag);
+
+	//タグの重複確認
+	if (texturePool.count(tagStr) != 0)
+		SAFE_RELEASE(texturePool[tagStr]);
+
+	//テクスチャロード
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+	D3DXCreateTextureFromFile(pDevice, path, &texturePool[tagStr]);
+}
+
+/**************************************
+テクスチャ解放処理
+***************************************/
+void ResourceManager::ReleaseTexture(const char* tag)
+{
+	string tagStr = string(tag);
+
+	//タグの登録確認
+	if (texturePool.count(tagStr) == 0)
+		return;
+
+	//メッシュコンテナを削除
+	SAFE_RELEASE(texturePool[tagStr]);
+	texturePool.erase(tagStr);
+}
+
+/**************************************
+メッシュ参照処理
+***************************************/
+bool ResourceManager::GetTexture(const char* tag, LPDIRECT3DTEXTURE9 *pOut)
+{
+	string tagStr = string(tag);
+
+	//登録確認
+	if (texturePool.count(tagStr) == 0)
+		return false;
+
+	//メッシュへの参照を格納
+	*pOut = texturePool[tagStr];
+	return true;
+}
+
+/**************************************
 全リソース解放処理
 ***************************************/
 void ResourceManager::AllRelease()
